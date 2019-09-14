8a9ce6746ddef166a60ccb399ad4bfcf8e16e7e9
johnced02 <46902679+johnced02@users.noreply.github.com>
2019-09-14 08:57:24 +0800

    Rename login.py

--- install2.sh
+++ install2.sh
@@ -1,12 +1,21 @@
-#!/data/data/com.termux/files/usr/bin/bash
-###############################################################################
-# Termux - Ubuntu                                                             #
-#                                                                             #
-#   ██╗  ██╗███╗   ███╗ ██████╗ ██████╗ ██████╗ ███████╗██████╗      #
-#   ╚██╗██╔╝████╗ ████║██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗   #
-#    ╚███╔╝ ██╔████╔██║██║   ██║██║  ██║██║  ██║█████╗  ██████╔╝     #
-#    ██╔██╗ ██║╚██╔╝██║██║   ██║██║  ██║██║  ██║██╔══╝  ██╔══██╗     #
-#   ██╔╝ ██╗██║ ╚═╝ ██║╚██████╔╝██████╔╝██████╔╝███████╗██║  ██║    #
-#   ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝     #
-#                                                                             #
-###############################################################################
+#!/data/data/com.termux/files/usr/bin/env python
+
+import getpass
+import hashlib
+import sys
+import os
+
+password = getpass.getpass()
+
+filepass = open("/data/data/com.termux/files/usr/bin/.pass", "r")
+filepass = filepass.read().split("\n")[0]
+
+password = password.encode()
+password = hashlib.sha1(password).hexdigest()
+
+if password != filepass:
+    print("Invalid password")
+    os.system("exit")
+else:
+    os.system("direct-ubuntu")
+    
